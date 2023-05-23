8a6f46007c6a01d780dbc6e807b9ba266e4b6619
Gabriel Tomás <95439214+gabriel-tomas@users.noreply.github.com>
2023-05-23 17:12:41 -0300

    ajeitando algumas coisas

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ menu.style["visibility"] = "hidden";
 function open_menu() {
     if (menu.style["visibility"] == "hidden") {
         document.querySelector("body > main").style["touch-action"] = "none";
-        document.querySelector("body").style["height"] = "max(100vh, 639.672px)";
+        document.querySelector("body").style["overflow-y"] = "hidden";
         menu.style["transform"] = "translateX(100vw)";
         setTimeout('menu.style["visibility"] = "visible"', 250);
         setTimeout('menu.style["transform"] = "translateX(0vw)"', 250);
@@ -17,6 +17,7 @@ function open_menu() {
     }
     else {
         document.querySelector("body > main").style.removeProperty("touch-action");
+        document.querySelector("body").style.removeProperty("overflow-y");
         menu.style["transform"] = "translateX(100vw)";
         document.getElementById("menu--item0").style["opacity"] = "0%";
         document.getElementById("menu--item1").style["opacity"] = "0%";
