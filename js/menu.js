39865f767267700b714d8a712be9d5a00f2bceab
Gabriel Tomás <95439214+gabriel-tomas@users.noreply.github.com>
2023-05-23 16:08:20 -0300

    menu transbordando da pagina problema

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,18 @@
 const menu_button = document.getElementById("menu-button");
 var menu = document.getElementById("menu");
-menu.style["visibility"] = "hidden"
+menu.style["visibility"] = "hidden";
 
 function open_menu() {
     if (menu.style["visibility"] == "hidden") {
-        menu.style["visibility"] = "visible";
-        menu.style["transform"] = "translateX(0vw)";
+        document.querySelector("body > main").style["touch-action"] = "none";
+        menu.style["transform"] = "translateX(100vw)";
+        setTimeout('menu.style["visibility"] = "visible"', 250);
+        setTimeout('menu.style["transform"] = "translateX(0vw)"', 250);
         setTimeout('document.getElementById("menu--item0").style["opacity"] = "100%"', 100);
         setTimeout('document.getElementById("menu--item1").style["opacity"] = "100%"', 100);
         setTimeout('document.getElementById("menu--item2").style["opacity"] = "100%"', 100);
-        setTimeout('document.querySelector("body > main").style["display"] = "none"', 250);
+        setTimeout('document.querySelector("body > main").style["display"] = "none"', 450);
+        
     }
     else {
         menu.style["transform"] = "translateX(100vw)";
@@ -18,5 +21,6 @@ function open_menu() {
         document.getElementById("menu--item2").style["opacity"] = "0%";
         document.querySelector("body > main").style["display"] = "block";
         setTimeout('menu.style["visibility"] = "hidden"', 250);
+        setTimeout('menu.style["transform"] = "translateX(0vw)"', 250);
     }
 }
