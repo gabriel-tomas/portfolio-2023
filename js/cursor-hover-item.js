0ef115cdcecd80412521cc579feb50f5dc397e7b
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-07-09 15:17:04 -0300

    hover class para quando protfolio item estiver hover

--- js/cursor-hover-item.js
+++ js/cursor-hover-item.js
@@ -1,13 +1,35 @@
-let page_link = document.querySelectorAll(".page-links > a");
+let page_link = document.querySelectorAll("a");
+let portfolio_item_image_container_link = document.querySelectorAll(".item--image-container");
+let portfolio_item_image_container_is_hovered = document.querySelectorAll(".image-container--is-hovered")
 let cursor_size = 50;
 let cursor_increased_size = `${cursor_size + (cursor_size * 0.54)}px`;
 
-page_link.forEach((item) => item.addEventListener("mouseover", function() {
+function mouseincrease() {
     cursor.style.width = cursor_increased_size;
     cursor.style.height = cursor_increased_size;
-}))
+}
 
-page_link.forEach((item) => item.addEventListener("mouseleave", function() {
+function mousedefault() {
     cursor.style.width = `${cursor_size}px`;
     cursor.style.height = `${cursor_size}px`;
-}))
+}
+
+function cursor_portfolio_item_hover() {
+    cursor.classList.add("portfolio-item-hover");
+}
+
+function cursor_remove_portfolio_item_hover() {
+    cursor.classList.remove("portfolio-item-hover");
+}
+
+page_link.forEach((item) => item.addEventListener("mouseover", mouseincrease));
+
+page_link.forEach((item) => item.addEventListener("mouseleave", mousedefault));
+
+portfolio_item_image_container_link.forEach((item) => item.addEventListener("mouseover", cursor_portfolio_item_hover));
+
+portfolio_item_image_container_link.forEach((item) => item.addEventListener("mouseleave", cursor_remove_portfolio_item_hover));
+
+portfolio_item_image_container_is_hovered.forEach((item) => item.addEventListener("mouseover", mouseincrease));
+
+portfolio_item_image_container_is_hovered.forEach((item) => item.addEventListener("mouseleave", mousedefault));
