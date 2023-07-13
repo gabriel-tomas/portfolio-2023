9ea725880fc30792369d93b7c321542ffb9d0636
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-07-13 15:53:56 -0300

    scroll x bug when close menu fixed

--- js/menu.js
+++ js/menu.js
@@ -38,11 +38,12 @@ function open_menu() {
     }
     else if (window.matchMedia("(max-width: 768px)").matches) {
         if (menu.style["visibility"] == "hidden") {
+            menu.style["display"] = "flex";
             document.querySelector("body > main").style["touch-action"] = "none";
             document.querySelector("body").style["overflow-y"] = "hidden";
-
+            
             document.body.style.top = `-${window.scrollY}px`;
-            document.body.style.position = 'fixed';
+            setTimeout("document.body.style.position = 'fixed'", 300)
             
             menu.style["transform"] = "translateX(100vw)";
             setTimeout('menu.style["visibility"] = "visible"', 250);
@@ -59,18 +60,19 @@ function open_menu() {
         else {
             document.querySelector("body > main").style.removeProperty("touch-action");
             document.querySelector("body").style.removeProperty("overflow-y");
-
+            
             const scrollY = document.body.style.top;
             document.body.style.position = '';
             document.body.style.top = '';
             window.scrollTo(0, parseInt(scrollY || '0') * -1);
-            
+
             menu.style["transform"] = "translateX(100vw)";
             document.getElementById("menu--item0").style["opacity"] = "0%";
             document.getElementById("menu--item1").style["opacity"] = "0%";
             document.getElementById("menu--item2").style["opacity"] = "0%";
             //document.querySelector("body > main").style["display"] = "block";
             setTimeout('menu.style["visibility"] = "hidden"', 250);
+            setTimeout('menu.style["display"] = "none"', 250);
             setTimeout('menu_behind.style["visibility"] = "hidden"', 250);
             menu_behind.style["opacity"] = "0";
             menu_behind.style["transition-delay"] = "0ms";
